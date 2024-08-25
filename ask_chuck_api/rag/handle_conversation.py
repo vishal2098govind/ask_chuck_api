71d2d64553e611db990d145b7d0fb941cc82496b
Vishal Govind <vishal.govind2098@gmail.com>
2024-08-25 18:35:04 +0530

    add response in conversation api

--- ask_chuck_api/rag/handle_conversation.py
+++ ask_chuck_api/rag/handle_conversation.py
@@ -5,7 +5,11 @@ from ask_chuck_api.rag.rag_chain import get_rag_chain
 async def handle_conversation(query: str, session_id: str):
     rag_chain = get_rag_chain()
 
-    rag_chain.invoke(
+    result = rag_chain.invoke(
         {"input": query},
         config={"configurable": {"session_id": session_id}}
     )
+
+    print('Context: ')
+    print(result['context'])
+    return result
